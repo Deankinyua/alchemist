ab65639cfa8702e0ed6a2cc7379c01b55713371b
deankinyua <kinyuadean@gmail.com>
2024-03-26 17:17:17 +0300

    learnt about changesets

--- linkly/lib/linkly/tags.ex
+++
@@ -1,9 +0,0 @@
-defmodule Linkly.Tag do
-  use Ecto.Schema
-
-  schema "tags" do
-    field(:title)
-
-    timestamps()
-  end
-end
--- linkly/lib/linkly/user.ex
+++ linkly/lib/linkly/user.ex
@@ -1,15 +1,27 @@
 defmodule Linkly.User do
   use Ecto.Schema
-  alias Linkly.Bookmark
+  import Ecto.Changeset
+  alias Linkly.{Bookmark, Link, LinkTag, Tag, User}
 
   schema "users" do
     field(:about, :string)
+    field(:birthdate, :date, virtual: true)
     field(:email, :string)
     field(:username, :string)
     has_many(:bookmarks, Bookmark)
     has_many(:bookmarked_links, through: [:bookmarks, :link])
+    has_many(:taggings, LinkTag)
+    many_to_many(:tagged_links, Link, join_through: LinkTag)
+    many_to_many(:tags, Tag, join_through: LinkTag)
 
     timestamps()
+  end
+
+  def changeset(%User{} = user, attrs) do
+    user
+     |> cast(user, attrs, [:username, :email, :birthdate, :about])
+     |> validate_required([[:username, :email, :birthdate]])
+     |> validate_length(:username, min: 3)
   end
 end
 
